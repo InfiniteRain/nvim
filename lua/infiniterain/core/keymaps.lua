1a319c9eea9bf8d8c5be0fe315478703e1efedc5
InfiniteRain <lysenkodavid@gmail.com>
2022-12-02 10:41:37 +0200

    Initial commit

---
+++ lua/infiniterain/core/keymaps.lua
@@ -0,0 +1,52 @@
+local keymap = vim.keymap
+
+---
+-- General keymaps
+---
+
+-- leader key
+vim.g.mapleader = " "
+
+-- clear search results
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- save file
+keymap.set("n", "<leader>w", ":w<CR>")
+
+-- clear single character
+keymap.set("n", "x", '"_x')
+
+-- increment number
+keymap.set("n", "<leader>+", "<C-a>")
+
+-- decrement number
+keymap.set("n", "<leader>-", "<C-x>")
+
+-- split vertically
+keymap.set("n", "<leader>sv", "<C-w>v")
+
+-- split horizontally
+keymap.set("n", "<leader>sh", "<C-w>s")
+
+-- equalize split windows
+keymap.set("n", "<leader>se", "<C-w>=")
+
+-- close current split window
+keymap.set("n", "<leader>sx", ":close<CR>")
+
+---
+-- Plugin keymaps
+---
+
+-- maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
