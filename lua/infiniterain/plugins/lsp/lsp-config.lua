2c65a79564b0f0a593caf6370830d7a5a6fccf39
David Lysenko <lysenkodavid@gmail.com>
2023-03-14 21:40:40 +0200

    Fix breaking changes and deprecations

--- lua/infiniterain/plugins/lsp/lsp-config.lua
+++ lua/infiniterain/plugins/lsp/lsp-config.lua
@@ -71,7 +71,7 @@ lspconfig["emmet_ls"].setup({
 	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
 })
 
-lspconfig["sumneko_lua"].setup({
+lspconfig["lua_ls"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = {
