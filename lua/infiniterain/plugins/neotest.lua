91e5dab4f53ac6f1b1e71d8339d611f6d53b5c98
InfiniteRain <lysenkodavid@gmail.com>
2023-03-25 20:09:34 +0200

    Change Jest's CWD to return path to config

--- lua/infiniterain/plugins/neotest.lua
+++ lua/infiniterain/plugins/neotest.lua
@@ -8,8 +8,59 @@ if not neotest_jest_setup then
 	return
 end
 
+function is_jest_config(filename)
+	local check_table = {
+		["jest.config.js"] = true,
+		["jest.config.ts"] = true,
+		["jest.config.mjs"] = true,
+		["jest.config.cjs"] = true,
+		["jest.config.json"] = true,
+	}
+
+	return check_table[filename] or false
+end
+
+function strip_last_segment(path)
+	local new_path = path:gsub("(.*)/.*$", "%1")
+
+	if path == new_path then
+		return false
+	end
+
+	return true, new_path
+end
+
+function find_jest_config_directory(path)
+	local success, new_path = true, path
+	repeat
+		success, new_path = strip_last_segment(new_path)
+
+		local paths = vim.split(vim.fn.glob(new_path .. "/*"), "\n")
+
+		for _, file in pairs(paths) do
+			local filename = file:sub(string.find(file, "/[^/]*$") + 1)
+
+			if is_jest_config(filename) then
+				return true, new_path
+			end
+		end
+	until not success
+
+	return false
+end
+
 neotest.setup({
 	adapters = {
-		neotest_jest({}),
+		neotest_jest({
+			cwd = function(path)
+				local is_success, directory = find_jest_config_directory(path)
+
+				if is_success then
+					return directory
+				end
+
+				return vim.fn.getcwd()
+			end,
+		}),
 	},
 })
