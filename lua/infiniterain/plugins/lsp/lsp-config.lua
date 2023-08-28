87dea00bc350ea252d81eac9b826a1fca7326e1d
David Lysenko <david.ly@miro.com>
2023-08-28 21:35:03 +0200

    Fix jump to definition

--- lua/infiniterain/plugins/lsp/lsp-config.lua
+++ lua/infiniterain/plugins/lsp/lsp-config.lua
@@ -12,7 +12,7 @@ require("infiniterain.util").safe_require(
 			-- set keybinds
 			keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
 			keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- go to declaration
-			keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+			keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- see definition and make edits in window
 			keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
 			keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
 			keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
