628d152e9e935e6893503accfb1785c18db5ac1b
David Lysenko <david.ly@miro.com>
2023-11-28 09:55:19 +0100

    Make harpoon's window larger

--- lua/infiniterain/plugins/harpoon.lua
+++ lua/infiniterain/plugins/harpoon.lua
@@ -1,3 +1,7 @@
 require("infiniterain.util").safe_require("harpoon", function(harpoon)
-	harpoon.setup()
+	harpoon.setup({
+		menu = {
+			width = vim.api.nvim_win_get_width(0) - 24,
+		},
+	})
 end)
