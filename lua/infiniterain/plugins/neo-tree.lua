497f50e4527c22ebda0bbf9349ac52ac7d9d661a
David Lysenko <lysenkodavid@gmail.com>
2023-12-03 17:00:48 +0100

    Delete collapse/expand chars from neo-tree

--- lua/infiniterain/plugins/neo-tree.lua
+++ lua/infiniterain/plugins/neo-tree.lua
@@ -4,8 +4,6 @@ require("infiniterain.util").safe_require("neo-tree", function(neo_tree)
 		default_component_configs = {
 			indent = {
 				with_expanders = true,
-				expander_collapsed = "",
-				expander_expanded = "",
 				expander_highlight = "NeoTreeExpander",
 			},
 		},
