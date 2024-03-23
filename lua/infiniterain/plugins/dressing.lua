8b980a965467db861387b2aca55ede9cfd2b06cd
David Lysenko <lysenkodavid@gmail.com>
2024-03-23 13:00:58 +0100

    Configure Dressing

--- lua/infiniterain/plugins/dressing.lua
+++ lua/infiniterain/plugins/dressing.lua
@@ -1,4 +1,15 @@
 return {
 	"stevearc/dressing.nvim",
 	event = "VeryLazy",
+	config = function()
+		local dressing = require("dressing")
+
+		dressing.setup({
+			input = {
+				insert_only = false,
+				start_in_insert = false,
+				border = "double",
+			},
+		})
+	end,
 }
